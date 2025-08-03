bb09e226599d24ac85c9162905eb80322af776a5
ixwelx <ridiixwelx@gmail.com>
2025-08-03 23:17:44 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let color =  prompt()
+let red = document.getElementById("red")
+let yellow = document.getElementById("yellow")
+let green = document.getElementById("green")
+
+ if (color === "красный" || color === "red") {
+    red.style.backgroundColor = "red";
+    document.getElementById("word").innerText = "STOP"
+} else if (color === "желтый" || color === "yellow") {
+    yellow.style.backgroundColor = "yellow";
+    document.getElementById("word").innerText = "WAIT"
+} else if (color === "зеленый" || color === "green") {
+    green.style.backgroundColor = "green";
+    document.getElementById("word").innerText = "GO"
+} else { "Ошибка!"}
